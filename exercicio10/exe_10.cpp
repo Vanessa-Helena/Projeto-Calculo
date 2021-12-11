9a436e6b7194ecd467073039fd8f3d52f294bbe9
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-12-11 20:54:21 -0300

    iniciando calculo 10

---
+++ exercicio10/exe_10.cpp
@@ -0,0 +1,7 @@
+/*Pretende-se estender um cabo de uma usina de força `a margem de um rio de 900 m de largura até uma fábrica situada do outro lado do rio, 3000 m rio abaixo. O custo para estender um cabo pelo rio é de R$ 5, 00 o metro, enquanto que para estendê-lo por terra custa R$ 4, 00 o metro. Qual é o percurso mais econômico para o cabo?
+
+ALTURA = 900m (largura do rio)
+COMPRIMENTO = 3000 (altura do rio)
+CUSTORIO = 5,00 O METRO (estender no rio)
+CUSTOTERRA = 4,00 METRO (estender terra)
+*/
